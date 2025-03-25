322aa35d9ef3ea02d3a3a66f896537d6c44b427a
ArjonD <bhm527@myy.haaga-helia.fi>
2025-03-25 16:47:28 +0200

    repository commit

---
+++ src/main/java/hh/lemmikkikauppa/lemmikkikauppaprojekti/LemmikkikauppaprojektiApplication.java
@@ -0,0 +1,13 @@
+package hh.lemmikkikauppa.lemmikkikauppaprojekti;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LemmikkikauppaprojektiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LemmikkikauppaprojektiApplication.class, args);
+	}
+
+}
