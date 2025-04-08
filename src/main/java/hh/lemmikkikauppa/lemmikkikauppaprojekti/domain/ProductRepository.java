c1c5e461922a0629fd80c90e9324178074d70c19
bhm530@myy.haaga-helia.fi <bhm530@myy.haaga-helia.fi>
2025-04-08 15:25:42 +0300

    productrepository

--- src/main/java/hh/lemmikkikauppa/lemmikkikauppaprojekti/domain/ProductRepository.java
+++ src/main/java/hh/lemmikkikauppa/lemmikkikauppaprojekti/domain/ProductRepository.java
@@ -1,6 +1,9 @@
 package hh.lemmikkikauppa.lemmikkikauppaprojekti.domain;
 
+import java.util.List;
 import org.springframework.data.repository.CrudRepository;
 
 public interface ProductRepository extends CrudRepository<Product, Long> {
+    List<Product> findByType(String type);
+
 }
