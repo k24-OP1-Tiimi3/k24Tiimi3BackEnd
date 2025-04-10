8ff603a736933cd90790dc02991b856b9b8c317a
ArjonD <bhm527@myy.haaga-helia.fi>
2025-04-10 19:09:17 +0300

    .requestMatchers(/api/**).permitAll() lisätty jotta api fetch toimii

--- src/main/java/hh/lemmikkikauppa/lemmikkikauppaprojekti/WebSecurityConfig.java
+++ src/main/java/hh/lemmikkikauppa/lemmikkikauppaprojekti/WebSecurityConfig.java
@@ -24,6 +24,7 @@ public class WebSecurityConfig {
         http
             .authorizeHttpRequests(authorize -> authorize
                 .requestMatchers("/login").permitAll()
+                .requestMatchers("/api/**").permitAll()
                 .anyRequest().hasRole("ADMIN")
             )
             .formLogin(formlogin -> formlogin
