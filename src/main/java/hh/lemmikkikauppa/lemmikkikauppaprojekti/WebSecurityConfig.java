f2e72d457e93c7da94c6a745c113fe93ce60ac5b
ArjonD <bhm527@myy.haaga-helia.fi>
2025-05-08 13:58:18 +0300

    cors configuration lisätty

--- src/main/java/hh/lemmikkikauppa/lemmikkikauppaprojekti/WebSecurityConfig.java
+++ src/main/java/hh/lemmikkikauppa/lemmikkikauppaprojekti/WebSecurityConfig.java
@@ -6,45 +6,58 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import java.util.Arrays;
 
 import hh.lemmikkikauppa.lemmikkikauppaprojekti.service.UserDetailServiceImpl;
-
 
 @Configuration
 public class WebSecurityConfig {
 
     private final UserDetailServiceImpl userDetailService;
 
-    public WebSecurityConfig(UserDetailServiceImpl userDetailService){
+    public WebSecurityConfig(UserDetailServiceImpl userDetailService) {
         this.userDetailService = userDetailService;
     }
 
     @Bean
     public SecurityFilterChain configure(HttpSecurity http) throws Exception {
         http
-            .authorizeHttpRequests(authorize -> authorize
-                .requestMatchers("/login").permitAll()
-                .requestMatchers("/api/**").permitAll()
-                .anyRequest().hasRole("ADMIN")
-            )
-            .formLogin(formlogin -> formlogin
-                .loginPage("/login")
-                .defaultSuccessUrl("/tuotteet", true)
-                .permitAll()
-            )
-            .logout( login -> login
-                .permitAll()
-            )
-            .csrf(csrf -> csrf.disable()) // Disable CSRF for H2 console
-            .headers(headers -> headers.frameOptions().disable()); // Allow H2 console to be displayed in a frame
-    
+                .cors(cors -> cors.configurationSource(corsConfigurationSource()))
+                .authorizeHttpRequests(authorize -> authorize
+                        .requestMatchers("/login").permitAll()
+                        .requestMatchers("/api/**").permitAll()
+                        .anyRequest().hasRole("ADMIN"))
+                .formLogin(formlogin -> formlogin
+                        .loginPage("/login")
+                        .defaultSuccessUrl("/tuotteet", true)
+                        .permitAll())
+                .logout(login -> login
+                        .permitAll())
+                .csrf(csrf -> csrf.disable()) // Disable CSRF for H2 console
+                .headers(headers -> headers.frameOptions().disable()); // Allow H2 console to be displayed in a frame
 
         return http.build();
     }
 
-
     @Bean
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration configuration = new CorsConfiguration();
+        configuration.setAllowedOrigins(Arrays.asList(
+                "http://localhost:5173",
+                "https://k24-op1-tiimi3.github.io/k24Tiimi3FrontEnd"));
+        configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
+        configuration.setAllowedHeaders(Arrays.asList("*"));
+        configuration.setAllowCredentials(true);
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+        return source;
     }
 }
